aad92456ac3b25c9c1c35d047927bd18f6130312
Eduardo Soares <eduardo21.net@gmail.com>
2025-05-07 19:32:16 -0300

    Adiciona script de Build e altera Our e Root de tsconfig

---
+++ api/server.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3333;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(routes_1.router);
+app.use((err, req, res, next) => {
+    if (err instanceof Error) {
+        res.status(400).json({
+            error: err.message,
+        });
+        return;
+    }
+    res.status(500).json({
+        status: "Error",
+        message: "Internal Server Error",
+    });
+    return;
+});
+app.listen(PORT, () => {
+    console.log(`Server is running locally at http://localhost:${PORT}`);
+});
