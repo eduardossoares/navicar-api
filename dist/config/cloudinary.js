a6fc6e105e23638fe4571853b3b287b004f9e099
Eduardo Soares <eduardo21.net@gmail.com>
2025-05-07 20:30:52 -0300

    Altera vercel.json

--- dist/config/cloudinary.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cloudinaryConfig = void 0;
-exports.cloudinaryConfig = {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-};
