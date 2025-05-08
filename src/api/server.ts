057b491b381ebe8d653bb53fa688f8e2d12da549
Eduardo Soares <eduardo21.net@gmail.com>
2025-05-08 14:15:42 -0300

    Instala micro-cors

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,9 @@
 import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
 import { router } from "../routes";
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://navicar-web.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
