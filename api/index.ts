92c71e0213742e4e8108738615c4f5c5b5a6e94a
Eduardo Soares <eduardo21.net@gmail.com>
2025-05-08 13:03:26 -0300

    Altera vercel.json e api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../src/api/server"; // ou onde estiver seu `app`
+import app from "../src/api/server";
+import { createServer } from "http";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+const server = createServer(app);
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res); // Express lida com isso por baixo dos panos
+  server.emit("request", req, res);
 }
